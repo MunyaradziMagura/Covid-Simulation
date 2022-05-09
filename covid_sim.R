dacab151675f90970efbec812c460ddfec907c59
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-05-09 10:30:09 +0930

    a person is born
    people can now be created

--- covid_sim.R
+++ covid_sim.R
@@ -5,19 +5,21 @@ if(!require('hash')) {
 }
 # Population size 
 numPopulation <- 100
+
 # individuals in population
-# each person will have a key i.e. 1 and a value [False,False,False]
-# the first index of the value represents a persons vitality, True = Healthy & False = Sick
-# the second index represents a persons life, True = Alive & False = Dead 
-# the third index represents a persons immunisation status, True = immune & False = Not Immune
+# each person will have a key i.e. 1 and a value [Healthy,Alive,Not Immune]
+# the first index of the value represents a persons vitality, Healthy or Sick
+# the second index represents a persons life, Alive or Dead 
+# the third index represents a persons immunisation status, Immune or Not Immune
 people <- hash()
 
 # create a person for every individual in the population
 inilisePeople <- function(populationSize) {
   for(persons in 0:populationSize){
     # people begin life, Healthy, Alive & Not Immune
-    people[persons] <- []
+    people[persons] <- list("Healthy","Alive","Not Immune")
   }
 }
 
-inilisePeople(numPopulation) # call the function named my_function
+inilisePeople(numPopulation) # create people
+print(people)
