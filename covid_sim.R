f455c29ceae6f557087c388e2d0dad7bb90947a3
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-05-09 12:19:40 +0930

    added sick people

--- covid_sim.R
+++ covid_sim.R
@@ -16,17 +16,33 @@ people <- hash()
 # how many people are infected with the virus at the start of the pandemic?
 patientZero <- 2
 
+# Vaccine rollout day (must be less than pandemic period). Patients can only be immunised after this date 
+vaccineRollOut = 20
 # virus invect
 
 # create a person for every individual in the population
 inilisePeople <- function(populationSize) {
+  
   for(persons in 1:populationSize + 1){
     # people begin life, Healthy, Alive & Not Immune
     people[persons] <- c("Healthy","Alive","Not Immune")
   }
+  
+  # infect patient zero/'s
+  for(victom in 1:patientZero){
+    # person infected
+    unlucky<-runif(1, min=1, max=populationSize)
+    unlucky<-round(unlucky)
+    infectPatient(unlucky)
+    print(values(people[toString(unlucky)]))
+  }
 }
 
+
+infectPatient <- function(patient){
+  values(people[toString(patient)])[1] <- "sick"
+}
+
+
 inilisePeople(numPopulation) # create people
-
-print(values(people["1"])[1]) # get specific value
-
+print(people)
