44984d65a3e864c0ea90c2b07198364417d8e372
Munyaradzi Magura <munyaradziMagura@outlook.com>
2022-05-10 16:39:28 +0930

    we can now create a dataframe of people

--- covidSimulation.R
+++ covidSimulation.R
@@ -2,30 +2,30 @@
 N_population = 10
 
 
+# a person has a random age between 1 & 99
+# a person is assigned a random gender 
+# a person is born healthy
+# a person is immunized 
+# a person has 0 Hospitalizations to begin with
+# create data frame 
+People <<- data.frame (
 
-People <- data.frame (
-  # 0 - 99
-  age <- c(), 
-  # [Other, Female, Male]
-  gender <- c(), 
-  #[healthy, sick, dead]
-  Vitality <- c(),
-  # True or False 
-  immunised <- c(),
-  # if a person has been hospitalized
-  Hospitalizations <- c()
+  age =  c(30),   # 0 - 99
+  
+  gender = c("Female"),   # [Female, Male]
+  
+  Vitality = c("Healthy"),  #[healthy, sick, dead]
+  
+  immunised = c("immunized"),  # True or False 
+  
+  Hospitalizations = c(0) # if a person has been hospitalized
 )
 
-# create data frame 
-#covid_Data Frame <- data.frame()
+
+# create people
 createPeople <- function(populationSize) {
-  for(person in 1:populationSize){
-    # a person has a random age between 1 & 99
-    # a person is assigned a random gender 
-    # a person is born healthy
-    # a person is immunized 
-    # a person has 0 Hospitalizations to begin with
-    
+  for(me in 1:populationSize){
+    People[nrow(People) + 1,] <<- c(as.numeric(round(runif(1, min=1, max=99))), if (round(runif(1, min=1, max=99)) %% 2) "Female" else "Male","Healthy","immunized",as.numeric(0)) 
   }
 }
 # this function is for infecting healthy people with covid people
